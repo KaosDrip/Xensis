75d310940c36edfc9ba3daaa226915a1da5be346
Aspect <75816479+Aspect0002@users.noreply.github.com>
2021-04-29 16:51:35 +0200

    Added Auto-Updater

---
+++ updater.py
@@ -0,0 +1,15 @@
+import time,requests,os,traceback,sys,json
+
+with open("fortnite.py", encoding='utf-8') as f:
+    current = f.read().replace('“','"').replace('”','"')
+github = requests.get("https://raw.githubusercontent.com/KaosDrip/Xensis/master/fortnite_update.py").text.replace('“','"').replace('”','"')
+if current != github:
+    print('I found an update, downloading...')
+    os.remove("fortnite.py")
+    with open("fortnite.py", 'w') as f:
+        f.write(github)
+    print('Done, starting your bot now.')
+elif current == github:
+    print('No update!')
+os.chdir(os.getcwd())
+os.execv(os.sys.executable,['python','fortnite.py'])
