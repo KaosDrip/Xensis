c3c87b2832376cf78992af7e043a47c43629baad
Aspect <75816479+Aspect0002@users.noreply.github.com>
2021-09-17 19:09:21 +0200

    text change

--- updater.py
+++ updater.py
@@ -4,12 +4,12 @@ with open("fortnite.py", encoding='utf-8') as f:
     current = f.read().replace('“','"').replace('”','"')
 github = requests.get("https://raw.githubusercontent.com/KaosDrip/Xensis/master/fortnite_update.py").text.replace('“','"').replace('”','"')
 if current != github:
-    print('I found an update, downloading...')
+    print('Update found, downloading...')
     os.remove("fortnite.py")
     with open("fortnite.py", 'w') as f:
         f.write(github)
     print('Done, starting your bot now.')
 elif current == github:
-    print('No update!')
+    print('No update found.')
 os.chdir(os.getcwd())
 os.execv(os.sys.executable,['python','fortnite.py'])
