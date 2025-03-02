5af2997aa2927c22df87a5e6022a9d58fa904044
Aspect <75816479+Aspect2222@users.noreply.github.com>
2025-03-03 00:15:53 +0100

    advanced starting and installing

--- START BOT.bat
+++ START BOT.bat
@@ -1,5 +1,13 @@
 @echo off
-cls
 title Xensis Lobby Bot created by github.com/KaosDrip.
+
+ECHO Installing the required packages for the bot!
+TIMEOUT 3
+
+py -3 -m pip install -U -r requirements.txt
+
+CLS
+ECHO Starting the bot...
+
 py fortnite.py
 cmd /k
